26ceac8afad483cdc313b92fe6dc4c6600b1caca
changhwan77 <ckdghks9741@gmail.com>
2023-01-07 21:17:19 +0900

    search_page update

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:toss_assemble/mini_tabbar.dart';
 import 'package:toss_assemble/today_discovery_test.dart';
 import 'screens/search_screen.dart';
 import 'screens/setting_screen.dart';
 import 'screens/calendar_screen.dart';
-import 'models/transaction_list.dart';
-//import 'screens/today_discovery.dart';
 import 'screens/mystock.dart';
 import 'appbar_icon.dart';
 import 'battom_navigationbar.dart';
-import 'components/myStocks/popping_card.dart';
-//import 'tabs.dart';
 import 'components/constants.dart';
 import 'package:toss_assemble/components/tabs.dart';
-import 'package:toss_assemble/models/transaction.dart';
 
 class MyBehavior extends ScrollBehavior {
   @override
@@ -30,24 +24,28 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        textTheme: const TextTheme(
-            bodyText2:
-                TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
-        // scaffoldBackgroundColor: const Color.fromARGB(141, 0, 0, 0),
-        scaffoldBackgroundColor: kAllThemeColour,
-        appBarTheme: AppBarTheme(
-          color: kAllThemeColour,
-        ),
-      ),
-      routes: {
-        '/search_screen': (context) => SearchScreen('검색창'),
-        '/settings': (context) => const SettingScreen(),
-        '/calendars': (context) => const CalendarScreen(),
+    return GestureDetector(
+      onTap: () {
+        FocusManager.instance.primaryFocus?.unfocus();
       },
-      home: const StockPage(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          textTheme: const TextTheme(
+              bodyText2:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+          // scaffoldBackgroundColor: const Color.fromARGB(141, 0, 0, 0),
+          scaffoldBackgroundColor: kAllThemeColour,
+          appBarTheme: AppBarTheme(
+            color: kAllThemeColour,
+          ),
+        ),
+        routes: {
+          '/settings': (context) => const SettingScreen(),
+          '/calendars': (context) => const CalendarScreen(),
+        },
+        home: const StockPage(),
+      ),
     );
   }
 }
@@ -110,10 +108,7 @@ class _StockPageState extends State<StockPage>
         ),
       ),
       actions: const [
-        AppBarIcons(
-          routeNames: '/search_screen',
-          icon: Icons.search,
-        ),
+        SearchBtn(),
         AppBarIcons(
           routeNames: '/calendars',
           icon: Icons.check_box,
